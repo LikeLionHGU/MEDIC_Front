1f6fab1470cfa8078ecf3f56a04061f6112f6a7d
Hayoung <shalom99904@gmail.com>
2024-07-21 00:54:25 +0900

    feat: 맞춤형 건강해시태그 컴포넌트 완성

--- medic/src/Router.js
+++ medic/src/Router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import SignUpPage2 from "./pages/SignUpPage2";
+import MainPage from "./pages/MainPage";
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
         <Route path="/Medic/LoginPage" element={<LoginPage />} />
         <Route path="/Medic/SignUpPage" element={<SignUpPage />} />
         <Route path="/Medic/SignUpPage2" element={<SignUpPage2 />} />
+        <Route path="/Medic" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ medic/src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import HashtagSelect from "../components/MainPage/HashtagSelect";
+import Customization from "../components/MainPage/Customization";
+
+const MainPage = () => {
+  return (
+    <>
+      <HashtagSelect></HashtagSelect>
+      <Customization></Customization>
+    </>
+  );
+};
+
+export default MainPage;
